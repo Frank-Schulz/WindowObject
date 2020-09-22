ce7edb3f79ea4e31857a7a84ae320f6008612fc7
Frank Schulz <Frank-Schulz@live.nmit.ac.nz>
2020-09-22 12:39:55 +1200

    created height_width.js, renamed index.js > create_window.js

---
+++ scripts/height_width.js
@@ -0,0 +1,5 @@
+
+                // (property) outerHeight: number
+let h = window.outerHeight;
+let w = window.outerWidth;
+window.document.write(`Height and width of the window are ${h} and ${w}`)
